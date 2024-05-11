cd063c6cc4d8e347d9b8f7b7359c164c9d0355b8
bhuvanesh-hj <bhuvaneshhj@gmail.com>
2024-05-11 19:02:07 +0530

    edit grey strip

--- src/components/header/grey-strip.js
+++ src/components/header/grey-strip.js
@@ -1,41 +1,37 @@
-"use client"
+"use client";
 
-import Image from "next/image";
 import styles from "./grey-strip.module.css";
 
 const GreyStrip = () => {
 	return (
 		<div className={styles.greyStrip}>
-			<div className={styles.lorem}>
-				<Image
-					className={styles.loremImage}
-					alt="loremImage"
-					width={16}
-					height={16}
-					src="/header/element-4.svg"
-				/>
-				<div className={styles.text}>
-					<text>Lorem ipsum dolor</text>
-				</div>
-			</div>
-			<div className={styles.lorem}>
-				<Image
-					className={styles.loremImage}
-					alt="loremImage"
-					width={16}
-					height={16}
-					src="/header/element-4.svg"
-				/>
-				<div className={styles.text}>
-					<text>Lorem ipsum dolor</text>
-				</div>
-			</div>
-			<div className={styles.lorem}>
+			<div className={styles.lorem1}>
 				<img
-					className={styles.loremImage}
 					alt="loremImage"
-					width={12}
-					height={12}
+					width={16}
+					height={16}
+					src="/header/element-4.svg"
+				/>
+				<div className={styles.text}>
+					<text>Lorem ipsum dolor</text>
+				</div>
+			</div>
+			<div className={styles.lorem2}>
+				<img
+					alt="loremImage"
+					width={16}
+					height={16}
+					src="/header/element-4.svg"
+				/>
+				<div className={styles.text}>
+					<text>Lorem ipsum dolor</text>
+				</div>
+			</div>
+			<div className={styles.lorem3}>
+				<img
+					alt="loremImage"
+					width={16}
+					height={16}
 					src="/header/element-4.svg"
 				/>
 				<div className={styles.text}>
